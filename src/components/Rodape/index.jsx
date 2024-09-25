67c2de1ca440ab3d2f87a459a413b05aae2b767d
workSilas <worksilas102@gmail.com>
2024-09-25 10:06:27 -0300

    Rodapé

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -3,7 +3,10 @@ import './index.scss';
 export default function Rodape() {
     return (
         <div className="Rodape">
-
+            <h1>BLOCKBUSTER</h1>
+            <div className='line' />
+            <a href="">Políticas de Privacidade</a>
+            <p>2024©</p>
         </div>
     );
 }
